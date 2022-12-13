1590d7e79125774a7147265bf0df92f68cbf5522
Raj <mody.raj@outlook.com>
2022-12-13 10:08:38 -0500

    Initialized datetime and rtc header/source files

---
+++ include/datetime.h
@@ -0,0 +1,18 @@
+#ifndef __DATETIME_H
+#define __DATETIME_H
+
+class DataTime {
+    private:
+        uint8_t seconds;
+        uint8_t minutes;
+        uint8_t hours;
+        uint8_t days;
+        uint8_t months;
+        uint8_t years;
+
+    public:
+        DataTime()
+}
+
+
+#endif /* __DATETIME_H */
---
+++ include/rtc.h
@@ -0,0 +1,13 @@
+#ifndef __RTC_H
+#define __RTC_H
+
+#include <Wire.h>
+
+class RTC {
+    private:
+
+    public:
+    
+}
+
+#endif /*__RTC_H */
